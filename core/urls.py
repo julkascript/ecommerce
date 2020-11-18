16427a3de5b8452baadaf8cad923d675054e2ef5
- Sensei <julianprodanov@icloud.com>
2020-11-19 00:45:31 +0200

    add login and register pages

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from core.views import home_page, cart
+from core.views import home_page, cart, about, sign_in, sign_up
 
 urlpatterns = [
     path('', home_page, name='home'),
     path('cart/', cart, name='cart page'),
+    path('about/', about, name='about page'),
+    path('signin/', sign_in, name='sign in'),
+    path('signup/', sign_up, name='sign up'),
 ]
--- core/views.py
+++ core/views.py
@@ -7,3 +7,15 @@ def home_page(request):
 
 def cart(request):
     return render(request, 'cart.html')
+
+
+def about(request):
+    return render(request, 'about-page.html')
+
+
+def sign_in(request):
+    return render(request, 'sign-in-page.html')
+
+
+def sign_up(request):
+    return render(request, 'sign-up-page.html')
